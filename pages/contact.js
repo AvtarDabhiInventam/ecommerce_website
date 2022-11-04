c6569156fad823d8310544e9b3da6182e20e695e
Neel <neel@inventam.com>
2022-11-04 10:32:31 +0530

    contact us design

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactForm from '../component/common/ContactForm';
+
+const contact = () => {
+  return (
+    <div>
+        <ContactForm />
+    </div>
+  )
+}
+
+export default contact
